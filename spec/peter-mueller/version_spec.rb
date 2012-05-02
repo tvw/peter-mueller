b778e6433d812b6d7a5de0ef0aef40a102129783
Thomas Volkmar Worm <tvw@s4r.de>
2012-05-03 01:21:17 +0200

    Version set to 1.0.0.

--- spec/peter-mueller/version_spec.rb
+++ spec/peter-mueller/version_spec.rb
@@ -1,7 +1,7 @@
 require "spec_helper"
 
 describe "PeterMueller::VERSION::STRING" do
-  it "should be 0.0.0" do
-    PeterMueller::VERSION::STRING.should == "0.0.0"
+  it "should be 1.0.0" do
+    PeterMueller::VERSION::STRING.should == "1.0.0"
   end
 end
